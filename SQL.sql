b620e7aacee314441513a83da8b7acb361b4f26e
Alexis <alexisjrojas@hotmail.es>
2024-02-28 20:19:51 -0300

    cambios en la base de datos

--- SQL.sql
+++ SQL.sql
@@ -12,21 +12,27 @@ CREATE TABLE IF NOT EXISTS afiliados(
     last_name VARCHAR(50) NOT NULL,
     email_address VARCHAR(50) UNIQUE,
     phone_number VARCHAR(50),
-    created_at TIMESTAMP NOT NULL DEFAULT (NOW())
+    created_at TIMESTAMP NOT NULL DEFAULT (NOW()),
+    PRIMARY KEY(id_afiliado)
 );
 
 /* TABLE GRUPO FAMILIAR */
 CREATE TABLE IF NOT EXISTS grupoFamiliar(
 	id_grupoFamiliar INT NOT NULL,
     id_titular INT NOT NULL,
-    id_conyuge INT,
     created_at TIMESTAMP NOT NULL DEFAULT (NOW()),
+    PRIMARY KEY(id_grupoFamiliar),
+    FOREIGN KEY(id_titular) REFERENCES afiliados(id_afiliado)
 );
 
 /* TABLE GRUPO FAMILIAR - TABLE AFILIADO  */
 CREATE TABLE IF NOT EXISTS afiliado_grupoFamiliar(
-	id_grupoFamiliar INT  NOT NULL,
-    id_afiliado INT NOT NULL 
+	uid INT NOT NULL AUTO_INCREMENT,
+    id_grupoFamiliar INT  NOT NULL,
+    id_afiliado INT NOT NULL,
+    PRIMARY KEY(uid),
+    FOREIGN KEY(id_grupoFamiliar) REFERENCES grupoFamiliar(id_grupoFamiliar),
+    FOREIGN KEY(id_afiliado) REFERENCES afiliados(id_afiliado)
 ); 
 
 /* TABLE HISTORIAL MEDICO */
@@ -35,6 +41,17 @@ CREATE TABLE IF NOT EXISTS historial_medico(
     id_afiliado INT NOT NULL,
     id_grupoFamiliar INT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT (NOW()),
+    PRIMARY KEY(id_historial),
+    FOREIGN KEY(id_afilido) REFERENCES afiliados(id_afiliado),
+    FOREIGN KEY (id_grupoFamiliar) REFERENCES grupoFamiliar(id_grupoFamiliar)
+);
+
+CREATE TABLE IF NOT EXISTS historial_descripcion(
+	uid INT NOT NULL AUTO_INCREMENT,
+	id_historial INT NOT NULL,
+    descripcion VARCHAR(500) NOT NULL,
+    PRIMARY KEY(id_historial),
+    FOREIGN KEY(id_historial) REFERENCES historial_medico(id_historial)
 );
 
 /* TABLE PAGOS_FACTURAS*/
